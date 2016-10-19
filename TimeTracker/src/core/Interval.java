3087a7cff8cdf58cde8919224ee020a0c662eaa5
Vernon Stanley Albayeros Duarte <drkztan@users.noreply.github.com>
2016-10-20 00:31:29 +0200

    1st commit

---
+++ TimeTracker/src/core/Interval.java
@@ -0,0 +1,103 @@
+package core;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
+
+/** 
+ * la clase intervalo es la clase que observa el clock segun el patron de dise�o Observer
+ * durante la conometracion de la tarea en la variable duracion se guarda el intervalo que es la 
+ * diferencia entre la fecha inicial de la tarea y la fecha final. 
+ */
+public class Interval implements Observer, Serializable {
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Task fatherTask = null;  
+
+    private String name = new java.lang.String();
+
+	private Date endDate;
+
+	private Date startDate;
+
+	private long length = 0;
+
+	private String description = new java.lang.String();
+
+    public Interval(String nname, String ndescription, Task nfatherTask){
+    	this.fatherTask = nfatherTask;
+    	this.name = nname;
+    	this.description = ndescription;
+    	this.startDate = new Date();
+    	fatherTask.getIntervalList().add(this);
+    }
+    
+	/** 
+	 * informs the observers
+	 */    
+	public void update(Observable observable, Object date){
+		this.length= Clock.getIntervalLength() ;
+		Activity tempA = this.fatherTask;
+		while(tempA != null){
+			this.setEndDate(Clock.getCurrentDate());
+		    tempA.setEndDate(this.endDate);
+		    tempA.addLength(2000);
+		    tempA = tempA.getFather();
+		}
+	}
+	
+    public Task getFatherTask() {
+		return fatherTask;
+	}
+
+    public void setFatherTask(Task nfatherTask) {
+		this.fatherTask = nfatherTask;
+	}
+ 
+    public String getName() {
+		return name;
+	}
+
+    public void setName(String nname) {
+		this.name = nname;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date nendDate) {
+		this.endDate = nendDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date nstartDate) {
+		this.startDate = nstartDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String ndescription) {
+		this.description = ndescription;
+	}
+
+	public long getLength() {
+		return length;
+	}
+
+	public void setLength(long nlength) {
+		this.length = nlength;
+	}
+} 
---
+++ TimeTracker/src/core/SerializeData.java
@@ -0,0 +1,61 @@
+package core;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializeData {
+	
+	public static void saveData (Project root, String name){
+		
+		String filename = name.toLowerCase();
+		filename = filename + ".bin";
+		
+		try{
+			
+			FileOutputStream fos = new FileOutputStream(name);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(root);
+			oos.close();
+			fos.close();			
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	public static Project loadData (String name){		
+		String filename = name.toLowerCase();
+		filename = filename + ".bin";
+		Project root = null;
+		try {
+			
+			FileInputStream fis = new FileInputStream(filename);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			root = (Project) ois.readObject();
+			
+			ois.close();
+			fis.close();
+			
+		} catch (IOException ioe){
+			
+			System.out.print("File not found, creating a new project");
+			root = new Project();
+			
+		} catch (ClassNotFoundException ce) {
+			
+			ce.printStackTrace();
+			
+		}
+		
+	return root;	
+	
+	}
+	
+}
